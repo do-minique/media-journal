1ca07792ee9eb8dd5ba8db77d629ce3682c9a75b
do-minique <267985416+do-minique@users.noreply.github.com>
2026-04-01 12:34:23 +0300

    Config example file replaced with the actual config file, and .gitignore updated accordingly

--- mjournal/config.example.py
+++
@@ -1,7 +0,0 @@
-# This an example file for config.py
-# Copy this file as config.py, and add your own secret
-# You can create the secret with for example:
-# $ python3
-# >>> import secrets
-# >>> secrets.token_hex(16)
-secret_key = "your-secret-key-here"
