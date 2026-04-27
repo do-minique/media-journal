11b65bf4d6dc23e7d0660ff0603a455eecc9d0b3
do-minique <267985416+do-minique@users.noreply.github.com>
2026-04-27 15:38:53 +0300

    Added NOT NULL and data validation restrictions for database creation

--- mjournal/schema.sql
+++ mjournal/schema.sql
@@ -1,22 +1,22 @@
 CREATE TABLE Users (
     id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    password_hash TEXT
+    username TEXT NOT NULL UNIQUE CHECK (trim(username) <> ''),
+    password_hash TEXT NOT NULL
 );
 
 CREATE TABLE Mediatypes (
     id INTEGER PRIMARY KEY,
-    name TEXT
+    name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE Media (
     id INTEGER PRIMARY KEY,
-    name TEXT,
+    name TEXT NOT NULL,
     description TEXT,
-    release_year INTEGER,
-    mediatype_id INTEGER REFERENCES Mediatypes,
-    adder_id INTEGER REFERENCES Users,
-    date_added TIMESTAMP
+    release_year INTEGER CHECK (release_year >= 0),
+    mediatype_id INTEGER NOT NULL REFERENCES Mediatypes(id),
+    adder_id INTEGER NOT NULL REFERENCES Users(id),
+    date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Genres (
@@ -25,19 +25,24 @@ CREATE TABLE Genres (
 );
 
 CREATE TABLE Genrelist (
-    media_id INTEGER REFERENCES Media,
-    genre_id INTEGER REFERENCES Genres,
+    media_id INTEGER NOT NULL REFERENCES Media(id),
+    genre_id INTEGER NOT NULL REFERENCES Genres(id),
     PRIMARY KEY (media_id, genre_id)
 );
 
 CREATE TABLE Reviews (
     id INTEGER PRIMARY KEY,
-    media_id INTEGER REFERENCES Media,
-    user_id INTEGER REFERENCES Users,
-    rating INTEGER,
+    media_id INTEGER NOT NULL REFERENCES Media(id),
+    user_id INTEGER NOT NULL REFERENCES Users(id),
+    rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 10),
     comment TEXT,
-    date_added TIMESTAMP
+    date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO Mediatypes (id, name) VALUES
+(1, 'Book'),
+(2, 'Movie'),
+(3, 'TV show');
 
 INSERT INTO Genres (name) VALUES
   ('Fantasy'),
